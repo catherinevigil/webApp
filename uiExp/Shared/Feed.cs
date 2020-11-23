832be458378c7f443c6122998954cd19ae148bde
Eli Burnes <eliburnes@gmail.com>
2020-11-22 21:47:45 -0500

    first commit

---
+++ uiExp/Shared/Feed.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using uiExp.Shared;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+
+namespace uiExp.Shared
+{
+
+    public class Feed
+    {
+        public List<Conversation> FeedList = new List<Conversation>();
+        public String name = "i am a feed";
+
+        public Feed(String[] lines)
+        {
+            FeedList = Seperate(lines);
+        }
+
+        private List<Conversation> Seperate(string[] lines)
+        {
+            Console.WriteLine("seperate was called");
+            List<Conversation> ConvoList = new List<Conversation>();
+            int index = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(ConvoList.Count);
+                if (lines[i] == "")
+                {
+                    ConvoList.Add(new Conversation());
+                }
+                else if (lines[i].Contains(":"))
+                {
+                    var name = lines[i].Remove(lines[i].Length - 1);
+                    if (ConvoList.LastOrDefault().Conversants == null)
+                    {
+                        index = 1;
+                        ConvoList.LastOrDefault().Conversants.Add(name);
+                    }
+                    else
+                    {
+                        index = ConvoList.LastOrDefault().Conversants.IndexOf(name);
+                        if (index != -1)
+                            ConvoList.LastOrDefault().Conversants.Add(name);
+                        index = ConvoList.LastOrDefault().Conversants.Count - 1;
+                    }
+
+
+                    ConvoList.LastOrDefault().Groups.Add(new MessageGroup());
+                }
+                else
+                {
+                    ConvoList.Last().Groups.Last().Messages.Add(new Message(lines[i]));
+                }
+
+            }
+
+            return ConvoList;
+        }
+    }
+}
---
+++ uiExp/Shared/Message.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using uiExp.Shared;
+
+namespace uiExp.Shared
+{
+    public class Message
+    {
+        public String Text { get; }
+
+        public enum type
+        {
+            //1: link;
+            //2: image;
+            //3: plaintext;
+        }
+
+        public enum Reaction
+        {
+            //thumbsUp:1;
+        }
+
+        public Message(String text)
+        {
+            Text = text;
+        }
+    }
+
+    public class Conversation
+    {
+        public List<MessageGroup> Groups = new List<MessageGroup>();
+        public List<String> Conversants = new List<String>(); //left is conversant 0
+
+        public Conversation()
+        {
+
+        }
+
+        public void output()
+        {
+            foreach (var group in Groups)
+                foreach (var message in group.Messages)
+                    Console.WriteLine(message.Text);
+        }
+    }
+
+
+    public class MessageGroup
+    {
+        public Boolean Left { get; }
+        public List<Message> Messages = new List<Message>();
+
+        public MessageGroup()
+        {
+            //Random rand = new Random();
+            //Left = rand.Next() > (Int32.MaxValue / 2);
+            //Messages.Add(new Message("blah blah blah"));
+            //Messages.Add(new Message("blah blah blah"));
+
+        }
+
+        public MessageGroup(Boolean left)
+        {
+            //Left = left;
+            //Random rand = new Random();
+            //left = rand.Next() > (Int32.MaxValue / 2);
+            //Messages.Add(new Message("blah blah ah blahah blahah blahah blahah blahah blahah blahah blahblah"));
+            //Messages.Add(new Message(
+            //    "blah blah ah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahah blahblah"));
+
+        }
+
+    }
+}
+
