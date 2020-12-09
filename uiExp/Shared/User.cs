f45def799276e109546f572c2dbe58294badd835
sahilhandapanda <73502478+sahilhandapanda@users.noreply.github.com>
2020-12-09 23:48:34 +0200

    Added bio field

--- uiExp/Shared/User.cs
+++ uiExp/Shared/User.cs
@@ -7,50 +7,12 @@ namespace uiExp.Shared
         private int PhoneNumber;
         private String Name;
         private String ImageUrl;
-        private String FirstName;
-        private String Username;
-        private String Password;
-
-        private string[] Names;
-
+        private String Bio;
 
         public User(string name, String img)
         {
             Name = name;
             ImageUrl = img;
-            MakeFirstName();
-        }
-        public User(string name, String username, String password, String img)
-        {
-            this.Name = name;
-            this.ImageUrl = img;
-            this.Password = password;
-            this.Username = username;
-
-            MakeFirstName();
-        }
-
-        public void MakeFirstName()
-        {
-            this.Names = this.Name.Split(' ');
-            this.FirstName = this.Names[0];
-        }
-        public bool LogIn(string username, string password)
-        {
-            Console.WriteLine("in user class method, username:" + username);
-
-            if (!(this.Username == null || this.Password == null)) {
-                Console.WriteLine("inside user class method if statement");
-                if (this.Username == username && this.Password == password)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-        public String GetFirstName()
-        {
-            return this.FirstName;
         }
         public String GetName()
         {
@@ -59,6 +21,11 @@ namespace uiExp.Shared
         public String GetImg()
         {
             return this.ImageUrl;
+        }
+
+        public String GetBio()
+        {
+            return this.Bio
         }
     }
 
